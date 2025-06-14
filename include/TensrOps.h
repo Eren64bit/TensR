eda790794c619e04bdd1966d348771074ba6e514
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-06-14 10:58:39 +0300

    Enhance operator overloads in TensrOps; add error handling for division and implement operator- and operator* functionality

--- include/TensrOps.h
+++ include/TensrOps.h
@@ -2,5 +2,15 @@
 
 #include "Tensr.h"
 
+//***************************************************************Operator (+)
 template<typename T, DeviceType Device>
 Tensr<T, Device> operator+(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal);
+
+template<typename T,DeviceType Device, typename Scalar, typename = std::enable_if_t<std::is_convertible_v<Scalar, T>>>
+Tensr<T, Device> operator+(const Tensr<T, Device>& t, Scalar scalar);
+
+//***************************************************************END
+
+//***************************************************************Operator (-)
+template<typename T, DeviceType Device>
+Tensr<T, Device> operator-(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal);
--- src/TensrOps.cpp
+++ src/TensrOps.cpp
@@ -1,7 +1,7 @@
 #include "../include/TensrOps.h"
 
 
-
+//***************************************************************Operator (+)
 template<typename T, DeviceType Device>
 Tensr<T, Device> operator+(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal){
     if (lVal.shape().size() != rVal.shape().size()) {
@@ -28,8 +28,101 @@ Tensr<T, Device> operator+(const Tensr<T, Device>& t, Scalar scalar) {
     for (size_t i = 0; i < t.size(); i++) {
         res_data[i] = t_data[i] + static_cast<T>(scalar);
     }
-    return result
+    return result;
+}
+//***************************************************************END
+
+//***************************************************************Operator (-)
+template<typename T, DeviceType Device>
+Tensr<T, Device> operator-(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal) {
+    if (lVal.shape().size() != rVal.shape().size()) {
+        throw std::runtime_error("error:Unmatched Tensor Shapes");
+    }
+    Tensr<T, Device> result(lVal.shape());
+    auto& res_data = result.mutable_data();
+    const auto& l_data = lVal.data();
+    const auto& r_data = rVal.data();
+
+    for (size_t i = 0; i < l_data.size(); i++) {
+        res_data[i] = l_data[i] - r_data[i];
+    }
+    return result;
 }
 
+template<typename T,DeviceType Device, typename Scalar, typename = std::enable_if_t<std::is_convertible_v<Scalar, T>>>
+Tensr<T, Device> operator-(const Tensr<T, Device>& t, Scalar scalar) {
+    Tensr<T, Device> result(t.shape());
+    auto& res_data = result.mutable_data();
+    const auto& t_data = t.data();
 
+    for (size_t i = 0; i < t.size(); i++) {
+        res_data[i] = t_data[i] - scalar;
+    }
+    return result;
+}
+//***************************************************************END
 
+//***************************************************************Operator (*)
+template<typename T, DeviceType Device>
+Tensr<T, Device> operator*(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal) {
+    if (lVal.shape().size() != rVal.shape().size()) {
+        throw std::runtime_error("error:Unmatched Tensor Shapes");
+    }
+    Tensr<T, Device> result(lVal.shape());
+    auto& res_data = result.mutable_data();
+    const auto& l_data = lVal.data();
+    const auto& r_data = rVal.data();
+
+    for (size_t i = 0; i < l_data.size(); i++) {
+        res_data[i] = l_data[i] * r_data[i];
+    }
+    return result;
+}
+
+template<typename T,DeviceType Device, typename Scalar, typename = std::enable_if_t<std::is_convertible_v<Scalar, T>>>
+Tensr<T, Device> operator*(const Tensr<T, Device>& t, Scalar scalar) {
+    Tensr<T, Device> result(t.shape());
+    auto& res_data = result.mutable_data();
+    const auto& t_data = t.data();
+
+    for (size_t i = 0; i < t.size(); i++) {
+        res_data[i] = t_data[i] * scalar;
+    }
+    return result;
+}
+//***************************************************************END
+
+//***************************************************************Operator (/)
+template<typename T, DeviceType Device>
+Tensr<T, Device> operator/(const Tensr<T, Device>& lVal, const Tensr<T, Device>& rVal) {
+    if (lVal.shape().size() != rVal.shape().size()) {
+        throw std::runtime_error("error:Unmatched Tensor Shapes");
+    }
+    Tensr<T, Device> result(lVal.shape());
+    auto& res_data = result.mutable_data();
+    const auto& l_data = lVal.data();
+    const auto& r_data = rVal.data();
+
+    for (size_t i = 0; i < l_data.size(); i++) {
+        if (r_data[i] == 0) {
+            throw std::runtime_error("Cannot divide with zero");
+        }
+        res_data[i] = l_data[i] / r_data[i];
+    }
+    return result;
+}
+
+template<typename T,DeviceType Device, typename Scalar, typename = std::enable_if_t<std::is_convertible_v<Scalar, T>>>
+Tensr<T, Device> operator/(const Tensr<T, Device>& t, Scalar scalar) {
+    Tensr<T, Device> result(t.shape());
+    auto& res_data = result.mutable_data();
+    const auto& t_data = t.data();
+
+    for (size_t i = 0; i < t.size(); i++) {
+        if (scalar == 0) {
+            throw std::runtime_error("Cannot divide with zero");
+        }
+        res_data[i] = t_data[i] / scalar;
+    }
+    return result;
+}
