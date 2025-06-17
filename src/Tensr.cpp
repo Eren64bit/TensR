445aff0548ece4083691ba4a809958f33809a285
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-06-17 22:24:11 +0300

    Refactor constructor initialization in Tensr class for clarity; add at() method in TensrLens class for element access

--- src/Tensr.cpp
+++ src/Tensr.cpp
@@ -6,9 +6,9 @@
 template<typename T>
 Tensr<T>::Tensr(std::vector<size_t> shape) 
     : shape_(std::move(shape))  {
-    this->total_size_ = compute_total_size(shape);
-    this->stride_ = compute_strides(shape);
-    this->rank_ = compute_rank(shape);
+    total_size_ = compute_total_size(shape);
+    stride_ = compute_strides(shape);
+    rank_ = compute_rank(shape);
 
     data_ptr_ = std::make_shared<std::vector<T>>(total_size_);
     std::fill(data_ptr_->begin(), data_ptr_->end(), 0);
@@ -18,14 +18,14 @@ template<typename T>
 Tensr<T>::Tensr(std::vector<size_t> shape, std::vector<value_type> data) 
     : shape_(std::move(shape)) {
     data_ptr_ = std::make_shared<std::vector<T>>(std::move(data));
-    this->total_size_ = compute_total_size(shape);
+    total_size_ = compute_total_size(shape);
 
     if (total_size_ != data_ptr_->size()) {
         throw std::runtime_error("Data and Shape does not match up");
     }
     
-    this->stride_ = compute_strides(shape);
-    this->rank_ = compute_rank(shape);
+    stride_ = compute_strides(shape);
+    rank_ = compute_rank(shape);
 
 
 }
