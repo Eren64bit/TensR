4dcdec81ff63180e89e9b1547c120ac77090129b
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-06-25 18:36:18 +0300

    Enhance transpose functions in TensrOps; add permutation parameter for flexible axis manipulation and improve error handling for axis validation.

--- include/TensrOps.hpp
+++ include/TensrOps.hpp
@@ -3,18 +3,53 @@
 #include "tensrLens.hpp"
 #include "tensr.hpp"
 
-/*
+
 namespace TensrOps {
 
     template<typename T>
-    tensrLens::lens<T> transpose(const tensr::Tensr<T>& tensor) {
+    tensrLens::lens<T> transpose(const tensr::Tensr<T>& tensor, const std::vector<size_t>& perm) {
+        if (perm.size() != tensor.rank()) {
+            throw std::invalid_argument("lens::transpose: axes size must match tensor rank");
+        }
 
+        std::vector<bool> used(tensor.rank(), false);
+        for (size_t axis : perm) {
+            if (axis >= tensor.rank() || used[axis]) {
+                throw std::invalid_argument("lens::transpose: invalid axes permutation");
+            }
+            used[axis] = true;
+        }
+
+        std::vector<size_t> new_shape, new_stride;
+        for (size_t axis : perm) {
+            new_shape.push_back(tensor.shape()[axis]);
+            new_stride.push_back(tensor.stride()[axis]);
+        }
+
+        return tensrLens::lens<T>(tensor.data(), new_shape, new_stride, tensor.offset());
     }
 
     template<typename T>
-    tensrLens::lens<T> transpose(const tensrLens::lens<T>& lens) {
-        
+    tensrLens::lens<T> transpose(const tensrLens::lens<T>& lens, const std::vector<size_t>& perm) {
+        if (perm.size() != lens.rank()) {
+            throw std::invalid_argument("lens::transpose: axes size must match tensor rank");
+        }
+
+        std::vector<bool> used(lens.rank(), false);
+        for (size_t axis : perm) {
+            if (axis >= lens.rank() || used[axis]) {
+                throw std::invalid_argument("lens::transpose: invalid axes permutation");
+            }
+            used[axis] = true;
+        }
+
+        std::vector<size_t> new_shape, new_stride;
+        for (size_t axis : perm) {
+            new_shape.push_back(lens.shape()[axis]);
+            new_stride.push_back(lens.stride()[axis]);
+        }
+
+        return tensrLens::lens<T>(lens.data(), new_shape, new_stride, lens.offset());
     }
 }
 
-*/
