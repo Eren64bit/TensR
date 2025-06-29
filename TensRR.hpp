21f7af7dcdd1164e4d59944668d55f1c4b6dcf67
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-06-29 17:07:30 +0300

    added umbrella header

---
+++ TensRR.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "include/tensr.hpp"
+#include "include/tensrLens.hpp"
+#include "include/tensrOps_decl.hpp"
+#include "include/tensrOps_imp.hpp"
+#include "include/tensrBroadcast.hpp"
+#include "include/tensrLazy.hpp"
+#include "include/tensrSugar.hpp"
