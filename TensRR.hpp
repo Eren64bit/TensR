bed94f02d7937aa1aa2f8bfee705c4e012121670
Eren64bit <ozcomlekciahmeteren@gmail.com>
2025-06-30 12:52:16 +0300

    fresh start for cudaKernels

--- TensRR.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "include/tensr.hpp"
-#include "include/tensrLens.hpp"
-#include "include/tensrOps_decl.hpp"
-#include "include/tensrOps_imp.hpp"
-#include "include/tensrBroadcast.hpp"
-#include "include/tensrLazy.hpp"
-#include "include/tensrSugar.hpp"
--- include/CUDA/cudaKernels.cu
+++
@@ -1,1 +0,0 @@
-#include "cudaKernels.cuh"
